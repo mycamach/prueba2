9b62eccbaa57e76bf3e02289da58ae3c53ac0c31
mycamach <manoli.camacho.a@gmail.com>
2017-09-02 16:08:06 -0300

    Menu ready

--- prueba2.rb
+++ prueba2.rb
@@ -2,28 +2,30 @@ file = File.open('notas.csv', 'r')
 notas = file.read
 file.close
 
-print notas
-
 puts 'Sistema de notas'
 
 input = 0
-while input != 4
-  puts 'Ingrese 1 para generar archivos por alumno'
-  puts 'Ingrese 2 para mostrar las inasistencias totales'
-  puts 'Ingrese 3 para mostrar los alumnos aprobados'
-  puts 'Ingrese 4 para salir'
 
-  input = gets.chomp
+while input
+  puts 'Ingrese 1 para generar archivos por alumno.'
+  puts 'Ingrese 2 para mostrar las inasistencias totales.'
+  puts 'Ingrese 3 para mostrar los alumnos aprobados.'
+  puts 'Ingrese 4 para salir.'
+
+  input = gets.chomp.to_i
 
   puts case input
        when 1
-         puts 'Elegiste la opción 1'
+         puts 'Elegiste la opción 1.'
        when 2
-         puts 'Elegiste la opción 2'
+         puts 'Elegiste la opción 2.'
        when 3
-         puts 'Elegiste la opción 3'
+         puts 'Elegiste la opción 3.'
        when 4
+         puts '¡Hasta pronto!'
          exit
+       else
+         puts 'La opción no es válida. Vuelve a ingresar un número.'
        end
 end
 
