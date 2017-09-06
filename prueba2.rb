3f2c271e1c9d08ce0a013dbda0b35bae6af935bf
mycamach <manoli.camacho.a@gmail.com>
2017-09-06 15:59:09 -0300

    Passing students shown with method 3

--- prueba2.rb
+++ prueba2.rb
@@ -1,16 +1,20 @@
-def grade_average(filename)
+def grade_average_file(filename)
 
   notas = File.open(filename, 'r').readlines
-
   notas.map do |ele|
     data = ele.split(', ').map(&:chomp)
     notas_alumno = data[1..5].delete_if { |n| n == 'A' }
     grades = notas_alumno.map(&:to_f)
-    suma = grades.inject(0) { |sum, x| sum + x }
+    suma = grades.inject(0) { |sum, num| sum + num }
     average = suma / grades.length
     File.open('alumnos.txt', 'a+'){ |file| file.puts "#{data[0]} tiene promedio #{average}" }
   end
 end
+
+#def average_file(filename)
+  #promedio = grade_average(filename).pop
+  #print "Mi promedio es #{promedio}"
+#end
 
 def total_assistance(filename)
   assistance = File.open(filename, 'r').readlines
@@ -28,14 +32,15 @@ def total_assistance(filename)
   end
 end
 
-def passing_students(filename)
-  data = File.open(filename, 'r').readlines
+def passing_students(nota)
+  data = File.open('notas.csv', 'r').readlines
   data.map do |datos|
-    alumnos = datos.split(', ').map(&:chomp)
-    nombre = alumnos.shift
-    print nombre
-  
-  
+    alumnos_notas = datos.split(', ').map(&:chomp)
+    alumnos = alumnos_notas[1..5].delete_if { |e| e == 'A' }
+    alumnos_final = alumnos.map(&:to_f)
+    sumatoria = alumnos_final.inject(0) { |suma, g| suma + g }
+    promedio = sumatoria / alumnos_final.length
+    puts "Alumno aprobado: #{alumnos_notas[0]}" if promedio > nota.to_f
   end
 end
 
@@ -53,14 +58,16 @@ while input
 
   puts case input
        when 1
-         grade_average('notas.csv')
+         grade_average_file('notas.csv')
          puts 'Se ha creado el archivo alumnos.txt en su directorio actual.'
          
        when 2
          total_assistance('notas.csv')
          
        when 3
-         passing_students('notas.csv')
+         puts 'Ingresa la nota de aprobación (ej: 5.0).'
+         passing_grade = gets
+         passing_students(passing_grade)
          
        when 4
          puts '¡Hasta pronto!'
